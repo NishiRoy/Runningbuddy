436495153356aa6661a9043ae4943e32ff1ee4e6
Rob_f <pv2fox@gmail.com>
2018-05-24 12:01:34 -0700

    added route component, fixed index.html

--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -7,13 +7,14 @@ import { HttpService } from './http.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import {AuthGuard} from './auth.guard';
+import { RootComponent } from './root-component/root-component.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     rountingcomponent,
-   
+    RootComponent,
   ],
   imports: [
     BrowserModule,
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
      host: 'localhost',
      user: 'root',
-     password: 'root',
+     password: 'Pa+rio+Bldg4100',
      database: 'mydb'
 });
 connection.connect();
