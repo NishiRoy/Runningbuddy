a0cf72aade4f8ba38bdf57183002d32ca893b917
Rob_f <pv2fox@gmail.com>
2018-05-24 13:26:01 -0700

    performed manual merge 1

--- angularApp/src/app/app-routing.module.ts
+++ angularApp/src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { EditScheduleComponent } from './edit-schedule/edit-schedule.component';
+import { GoRunningComponent } from './go-running/go-running.component';
 import { AuthGuard } from './auth.guard';
 import { RootComponent } from './root-component/root-component.component';
 
@@ -11,8 +14,11 @@ const routes: Routes = [
   {path: '', component: RootComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  {path: '**', component: PageNotFoundComponent}
+  {path:'home/:id',component:HomeComponent,canActivate:[AuthGuard],children:[
+  {path:'edit/:id',component:EditProfileComponent},
+  {path:'editschedule/:id',component:EditScheduleComponent},
+  {path:'gorunning/:id',component:GoRunningComponent}]},
+  {path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
